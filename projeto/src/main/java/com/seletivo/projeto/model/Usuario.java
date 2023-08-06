1ab2437df791ed5564fb1ac9d5e12ccbdddf07f4
Arthurnicolaul <132392293+Arthurnicolaul@users.noreply.github.com>
2023-08-06 13:19:09 -0300

    first commit

---
+++ projeto/src/main/java/com/seletivo/projeto/model/Usuario.java
@@ -0,0 +1,28 @@
+package com.seletivo.projeto.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String senha;
+    @Email
+    private String email;
+    private String nomeCompleto;
+    private String sexo;
+}
+
